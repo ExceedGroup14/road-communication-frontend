16ce920a730c92f2c8569f888a712e357fbce981
tboonma <tboonma@hotmail.com>
2022-02-19 12:27:01 +0700

    Improve Login page and request from API

--- src/components/Loginform.js
+++ src/components/Loginform.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import '../components/Loginform.css'
+import AuthMenubar from './AuthMenubar'
 
 const Loginform = (props) => {
   return (
     <div className="outer-container">
       <div id="circle"></div>
-      <div className="container login">
-        <div className="title">Road Communication</div>
-        <div className="">
-          <form className="loginform">
-            <input
-              placeholder="User ID"
-              value={props.Uid}
-              onChange={(e) => props.setUid(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={props.Pass}
-              onChange={(e) => props.setPass(e.target.value)}
-            />
-            <button
-              className="inputbutton"
-              onClick={props.onClick}
-              type="submit"
-            >
-              Login
-            </button>
-          </form>
+      <div className="container">
+        <AuthMenubar />
+        <div className="login">
+          <div className="title-section">
+            <h1 className="">
+              Text Communication <br /> On The Road
+            </h1>
+          </div>
+          <div className="form-section">
+            <form className="loginform">
+              <input
+                placeholder="User ID"
+                value={props.username}
+                onChange={(e) => props.setUsername(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={props.password}
+                onChange={(e) => props.setPassword(e.target.value)}
+              />
+              <button
+                className="inputbutton"
+                onClick={props.onClick}
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Loginform from '../components/Loginform'
+import axios from 'axios'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function onSubmit(e) {
+    e.preventDefault()
+    let data = {
+      username: username,
+      password: password,
+    }
+    const response = await axios.post(
+      'https://ecourse.cpe.ku.ac.th/exceed14/api/login/',
+      data
+    )
+    console.log(response.data)
+    alert('See data in console')
+  }
   return (
     <div className="loginpage">
-      <Loginform />
+      <Loginform
+        username={username}
+        password={password}
+        onClick={onSubmit}
+        setUsername={setUsername}
+        setPassword={setPassword}
+      />
     </div>
   )
 }
