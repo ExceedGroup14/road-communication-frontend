d042ad5737c8603d8a243eebb8f65cf6952d245a
JL-EFR <jompop.l@ku.th>
2022-02-19 12:29:09 +0700

    edit UI

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -1,13 +1,22 @@
 import React from "react"
 import Carname from "../components/Carname"
 import Carpic from "./car.png"
+import "./status.css"
 
 const Status = () => {
   return (
     <div className="container">
       <Carname />
       <div className="status">
+        <div className="leftst">
+          <h3>Left rear light</h3>
+          <p>Working</p>
+        </div>
         <img src={Carpic} alt="Car pic" />
+        <div className="rightst">
+          <h3>Right rear light</h3>
+          <p>Not Working</p>
+        </div>
       </div>
     </div>
   )
