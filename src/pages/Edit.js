5d06d6f333115b5dc1ef9e660849b86a8f56d32f
JL-EFR <jompop.l@ku.th>
2022-02-19 14:52:00 +0700

    fix all UI

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import Word from "../components/Word"
+import Carname from "../components/Carname"
+import "./edit.css"
 
 const Edit = () => {
-  return <div>Edit</div>
+  useEffect(() => {}, [])
+  return (
+    <div className="outer-container">
+      <div id="circle"></div>
+      <div className="container">
+        <Carname />
+        <div className="content">
+          <div className="wordlist">
+            <Word word="word" />
+            <Word word="word" />
+            <Word word="word" />
+            <Word word="word" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Edit
--- src/pages/Status.js
+++ src/pages/Status.js
@@ -5,17 +5,20 @@ import "./status.css"
 
 const Status = () => {
   return (
-    <div className="container">
-      <Carname />
-      <div className="status">
-        <div className="leftst">
-          <h3>Left rear light</h3>
-          <p>Working</p>
-        </div>
-        <img src={Carpic} alt="Car pic" />
-        <div className="rightst">
-          <h3>Right rear light</h3>
-          <p>Not Working</p>
+    <div className="outer-container">
+      <div id="circle"></div>
+      <div className="container">
+        <Carname />
+        <div className="status">
+          <div className="leftst">
+            <h3>Left rear light</h3>
+            <p>Working</p>
+          </div>
+          <img src={Carpic} alt="Car pic" />
+          <div className="rightst">
+            <h3>Right rear light</h3>
+            <p>Not Working</p>
+          </div>
         </div>
       </div>
     </div>
